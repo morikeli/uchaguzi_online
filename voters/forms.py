0ea007122c3face894d5f14d21b312cdfeadfd0a
bro_code <kelimbth@gmail.com>
2022-10-10 11:03:12 +0300

    added LoginForm, SignupForm

--- voters/forms.py
+++ voters/forms.py
@@ -1,4 +1,22 @@
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django import forms
+
+
+class LoginForm(AuthenticationForm):
+    def __init__(self, request=None, *args, **kwargs):
+        self.__init__(request, *args, **kwargs)
+
+        self.fields['username'].label = 'Name'
+        self.error_messages['invalid_login'] = 'INVALID CREDENTIALS!!! Name and password maybe case-senstitive.'
+
+
+class SignupForm(UserCreationForm):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'password1', 'password2']
 
--- voters/views.py
+++ voters/views.py
@@ -1,4 +1,29 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView, Logoutview
 from django.contrib.auth.decorators import login_required, user_passes_test
+from .forms import LoginForm, SignupForm
 
+class UserLogin(LoginView):
+    authentication_form = LoginForm
+    template_name = 'voters/login.html'
+
+
+def signup_view(request):
+    form = SignupForm()
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            new_voter = form.save(commit=False)
+            new_voter.username = new_voter.first_name+' '+new_voter.last_name
+            new_voter.save()
+
+            messages.success(request, 'New account created successfully!')
+            return redirect('voter_profile')
+
+    context = {'signup_form': form}
+    return render(request, 'voters/signup.html', context)
+
+
+
+class LogoutVoter(LogoutView):
+    template_name = 'voters/logout.html'
