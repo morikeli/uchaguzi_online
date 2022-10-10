b973b7b42a05b8d2819a6b3d827e9fc67333c674
bro_code <kelimbth@gmail.com>
2022-10-10 15:32:28 +0300

    makemigrations & register models in admin-panel

--- voters/admin.py
+++ voters/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
+from .models import Voters, Aspirants
 
-# Register your models here.
+admin.site.register(Voters)
+admin.site.register(Aspirants)
