6f5adf6acfc865bd152cbf4f9166721dce05f1a8
bro_code <kelimbth@gmail.com>
2022-10-11 14:37:11 +0300

    added a elections polling app

--- src/settings.py
+++ src/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'voters.apps.VotersConfig',
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
