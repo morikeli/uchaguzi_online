69a878a7aea5cccd9b538f6ef302fe3b10e1222f
bro_code <kelimbth@gmail.com>
2022-10-11 18:02:09 +0300

    created tables

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Polls, Polled
 
-# Register your models here.
+@admin.register(Polls)
+class Polls(admin.ModelAdmin):
+    list_display = ['name', 'total_polls']
+
+@admin.register(Polled)
+class PolledList(admin.ModelAdmin):
+    list_display = ['user_id', 'created']
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from voters.models import Aspirants, Voters
 
-# Create your models here.
+
+class Polls(models.Model):
+    id = models.CharField(max_length=15, primary_key=True, editable=False)
+    name = models.OneToOneField(Aspirants, on_delete=models.CASCADE, editable=False)
+    total_polls = models.PositiveIntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        return super(Polls, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'Polls'
+        ordering = ['total_polls']
+
+class Polled(models.Model):
+    id = models.CharField(max_length=15, editable=False, primary_key=True, unique=True)
+    user_id = models.CharField(max_length=20)
+    polled = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Polled'
