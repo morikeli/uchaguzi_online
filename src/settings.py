905dea2c39f73e282f3c6e02ae6bc45d701df87e
bro_code <kelimbth@gmail.com>
2022-10-12 13:17:40 +0300

    added and configured templates folder

--- src/settings.py
+++ src/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'src.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates', BASE_DIR, 'voters/templates', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
