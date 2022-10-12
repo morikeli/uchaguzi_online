0d14fd3bdcbca6c80799614188edaa9478b13d77
bro_code <kelimbth@gmail.com>
2022-10-12 14:11:48 +0300

    added & config. static files

--- src/settings.py
+++ src/settings.py
@@ -119,6 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/landing-page'),
+    os.path.join(BASE_DIR, 'static/voters-page'),
+
+]
 
 MEDIA_URL = 'mediaFiles/'
 MEDIA_ROOT = os.path.join(BASE_DIR/'media/')
