b86991471df25dd38db68fc02301850387eba7a6
bro_code <kelimbth@gmail.com>
2022-10-18 08:18:11 +0300

    config. form in views

--- polls/views.py
+++ polls/views.py
@@ -6,27 +6,13 @@ from .forms import PollingForm
 @login_required(login_url='voters_login')
 @user_passes_test(lambda user: user.is_staff is False)
 def polling_view(request):
-    form = PollingForm()
-    try:
-        obj = Polls.objects.get(id=pk)
-        if request.method == 'POST':
-            form = PollingForm(request.POST, instance=obj)
-            if form.is_valid():
-                voter = form.save(commit=False)
-                voter.total_polls += 1
-                
-                save_voter_details = Polled.objects.create(user_id=voter.id)
-                save_voter_details.save()
-                voter.save()
+    if request.method == 'POST':
+        form = request.POST['vote']
 
-                messages.success(request, 'You polled for this candidate. Results will be released soon.')
-                return redirect('voters_homepage')
+        
+
     
-    except Polls.DoesNotExist:
-        return redirect('voters_homepage')
-    
-    context = {'polling_form': form}
-    return render(request, 'voters/homepage.html', context)
+    return render(request, 'voters/homepage.html')
 
 @login_required(login_url='voters_login')
 @user_passes_test(lambda user: user.is_staff is False)
