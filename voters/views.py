5b36e9ca5086743679f3df8068bb71c6be46a0bc
bro_code <kelimbth@gmail.com>
2022-10-31 10:01:45 +0300

    added a new user dp

--- voters/views.py
+++ voters/views.py
@@ -117,7 +117,7 @@ def homepage_view(request):
 
     registered_voters = Voters.objects.filter(registered=True, school=request.user.voters.school)
     pollers = Polled.objects.all().count()
-    polls = Polls.objects.all().order_by('post', 'total_polls')
+    polls = Polls.objects.filter(name__name__school=request.user.voters.school).all().order_by('post', 'total_polls')
     total_aspirants = Aspirants.objects.filter(name__school=request.user.voters.school).count()
     blogs = Blog.objects.filter(blogger__name__school=request.user.voters.school, ).all().order_by('-written')
     polls_percentage = (pollers/registered_voters.count())*100
