464e64c3b98f4fc488bb80009cb7bbc1522ecfda
Mori Keli <kelimbth@gmail.com>
2022-11-15 20:53:37 +0300

    moved the func. to render charts to an external js file

---
+++ static/voters-page/js/charts.js
@@ -0,0 +1,74 @@
+// Line chart
+document.addEventListener("DOMContentLoaded", () => {
+    new ApexCharts(document.querySelector("#RegVotersChart"), {
+        series: [{
+        name: 'SSAES',
+        data: [31, 40, 28, 51, 42, 82, 56],
+        }, {
+        name: 'SOB',
+        data: [11, 32, 45, 32, 34, 52, 41]
+        }, {
+        name: 'INFOCOMS',
+        data: [15, 11, 32, 18, 9, 24, 11]
+        }],
+        chart: {
+        height: 350,
+        type: 'area',
+        toolbar: {
+            show: false
+        },
+        },
+        markers: {
+        size: 4
+        },
+        colors: ['#4154f1', '#2eca6a', '#ff771d'],
+        fill: {
+        type: "gradient",
+        gradient: {
+            shadeIntensity: 1,
+            opacityFrom: 0.3,
+            opacityTo: 0.4,
+            stops: [0, 90, 100]
+        }
+        },
+        dataLabels: {
+        enabled: false
+        },
+        stroke: {
+        curve: 'smooth',
+        width: 2
+        },
+        xaxis: {
+        type: 'datetime',
+        categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+        },
+        tooltip: {
+        x: {
+            format: 'dd/MM/yy HH:mm'
+        },
+        }
+    }).render();
+});
+
+// doughnut chart
+document.addEventListener("DOMContentLoaded", () => {
+    new Chart(document.querySelector('#votersChart'), {
+        type: 'doughnut',
+        data: {
+        labels: [
+            {% if male_reg_voters %}'Male(s)',{% endif %} {% if female_reg_voters %}'Female(s)'{% endif %}
+        ],
+        datasets: [{
+            label: 'Registered Voters',
+            data: [
+                {{male_reg_voters}}, {{female_reg_voters}}
+            ],
+            backgroundColor: [
+                '#09ac52',
+                '#ec0b0b',
+            ],
+            hoverOffset: 4
+        }]
+        }
+    });
+});
