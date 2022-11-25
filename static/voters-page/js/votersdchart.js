fcf2926fbb476713531ca6c7ab008cb3eafc5bee
Mori Keli <kelimbth@gmail.com>
2022-11-25 12:35:52 +0300

    added a few conditions

--- static/voters-page/js/votersdchart.js
+++ static/voters-page/js/votersdchart.js
@@ -1,13 +1,42 @@
 const MaleRegisteredVoters = document.getElementById("males").value;
 const FemaleRegisteredVoters = document.getElementById("females").value;
 
+// Male Registered Voters
+if (MaleRegisteredVoters == 0) {
+	var chartLabelForMales = ''
+	var colorMaleLabel = '#fff'
+} 
+else if (MaleRegisteredVoters == 1) {
+	var chartLabelForMales = 'Male'
+	var colorMaleLabel = '#09ac52'
+}
+else {
+	var chartLabelForMales = 'Males'
+	var colorMaleLabel = '#09ac52'
+
+}
+
+// Female Registered Voters
+if (FemaleRegisteredVoters == 0) {
+	var chartLabelForFemales = ''
+	var colorFemaleLabel = '#fff'
+} 
+else if (FemaleRegisteredVoters == 1) {
+	var chartLabelForFemales = 'Female'
+	var colorFemaleLabel = '#ec0b0b'
+}
+else {
+	var chartLabelForFemales = 'Females'
+	var colorFemaleLabel = '#ec0b0b'
+}
+
+
 document.addEventListener("DOMContentLoaded", () => {
 new Chart(document.querySelector('#votersChart'), {
 	type: 'doughnut',
 	data: {
 	labels: [
-		'Males',
-		'Females',
+		chartLabelForMales, chartLabelForFemales
 	],
 	datasets: [{
 		label: 'Registered Voters',
@@ -15,8 +44,8 @@ new Chart(document.querySelector('#votersChart'), {
 			MaleRegisteredVoters, FemaleRegisteredVoters, 
 		],
 		backgroundColor: [
-			'#09ac52',
-			'#ec0b0b',
+			colorMaleLabel,
+			colorFemaleLabel,
 		],
 		hoverOffset: 4
 	}]
