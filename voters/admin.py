9fd1ec46c496440fca3ca025d08f0ede9e9fc47f
Mori Keli <kelimbth@gmail.com>
2022-11-25 15:09:52 +0300

    customized admin panel

--- voters/admin.py
+++ voters/admin.py
@@ -4,16 +4,22 @@ from .models import Voters, Aspirants, Blog, Voted
 @admin.register(Voters)
 class VotersTable(admin.ModelAdmin):
     list_display = ['voter', 'gender', 'school', 'reg_no', 'year']
+    readonly_fields = ['gender', 'dob', 'age', 'school', 'reg_no', 'year', 'semester', 'phone_no']
+    fields = ['gender', 'dob', 'age', 'phone_no', 'school', 'reg_no', 'year', 'semester']
 
 @admin.register(Aspirants)
 class AspirantsTable(admin.ModelAdmin):
     list_display = ['name', 'post', 'slogan', 'nominate']
-    readonly_fields = ['alias', 'bio', 'post', 'slogan', 'pic', 'form', 'nominate',]
+    readonly_fields = ['alias', 'bio', 'post', 'slogan', 'pic', 'form',]
+    fields = ['alias', 'bio', 'post', 'slogan', 'pic', 'form', 'nominate']
 
 @admin.register(Blog)
 class BlogTable(admin.ModelAdmin):
     list_display = ['blogger', 'message', 'written']
+    readonly_fields = ['message']
 
 @admin.register(Voted)
 class VotedTable(admin.ModelAdmin):
-    list_display = ['id', 'academic', 'general_rep', 'ladies_rep', 'treasurer', 'governor', 'president']
+    list_display = ['id', 'academic', 'general_rep', 'ladies_rep', 'treasurer', 'governor', 'president', 'voted']
+    ordering = ['-voted']
+
