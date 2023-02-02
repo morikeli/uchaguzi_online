31976030b086d828e43f31b9b2ba3fc3d390903c
brocode <kelimbth@gmail.com>
2023-02-03 01:47:16 +0300

    created officials table

--- accounts/models.py
+++ accounts/models.py
@@ -9,7 +9,7 @@ class Voters(models.Model):
     phone_no = models.CharField(max_length=14, blank=False)
     age = models.PositiveIntegerField(default=0, editable=False)
     dob = models.DateField(null=True, blank=False)
-    profile_pic = models.ImageField(upload_to='VotersDps/', default='default.png')
+    profile_pic = models.ImageField(upload_to='Voters-Dps/', default='default.png')
     reg_no = models.CharField(max_length=14, blank=False)
     school = models.CharField(max_length=70, blank=False)
     year = models.CharField(max_length=12, blank=False)
@@ -34,4 +34,36 @@ class Voters(models.Model):
     class Meta:
         verbose_name_plural = 'Voters'
         ordering = ['voter']
+
+
+class Officials(models.Model):
+    id = models.CharField(max_length=15, primary_key=True, editable=False, unique=True)
+    officer = models.OneToOneField(User, on_delete=models.CASCADE, editable=False)
+    gender = models.CharField(max_length=7, blank=False)
+    phone_no = models.CharField(max_length=14, blank=False)
+    dob = models.DateField(null=True, blank=False)
+    profile_pic = models.ImageField(upload_to='Officials-Dps/', default='default.png')
+    school = models.CharField(max_length=70, blank=False)
+    role = models.CharField(max_length=25, blank=False)
+    is_official = models.BooleanField(default=False, editable=False)
+    registered = models.BooleanField(default=False, editable=False)
+    created = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{str(self.voter.username).title()}'
+
+    def save(self, *args, **kwargs):
+        super(Officials, self).save(*args, **kwargs)
+
+        dp = Image.open(self.profile_pic.path)
+
+        if dp.height > 400 and dp.width > 400:
+            output_size = (400, 400)
+            dp.thumbnail(output_size)
+            dp.save(self.profile_pic.path)
+
+    class Meta:
+        verbose_name_plural = 'Electoral Officials'
+        ordering = ['officer']
 
--- accounts/signals.py
+++ accounts/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import pre_save, post_save
 from django.contrib.auth.models import User
 from django.db.utils import IntegrityError
 from django.dispatch import receiver
-from .models import Voters
+from .models import Voters, Officials
 from datetime import datetime
 import uuid
 
@@ -33,8 +33,39 @@ def generate_voter_id(sender, instance, **kwargs):
         return
 
 
+@receiver(pre_save, sender=Officials)
+def generate_officer_id(sender, instance, **kwargs):
+    if instance.id == "":
+        instance.id = str(uuid.uuid4()).upper().replace('-', '')[:15]
+
+    try:
+        if datetime.now().strftime('%Y-%m-%d %H:%M:%S') > instance.created.strftime('%Y-%m-%d %H:%M:%S'):
+            officer_dob = str(instance.dob)
+            get_OfficerDob = datetime.strptime(officer_dob, '%Y-%m-%d')
+            current_date = datetime.now()
+            officer_age = current_date - get_OfficerDob
+            convert_OfficerAge = int(officer_age.days/365.25)
+            instance.age = convert_OfficerAge
+            
+        else:
+            officer_dob = str(instance.dob)
+            get_OfficerDob = datetime.strptime(officer_dob, '%Y-%m-%d')
+            current_date = datetime.now()
+            officer_age = current_date - get_OfficerDob
+            convert_OfficerAge = int(officer_age.days/365.25)
+            instance.age = convert_OfficerAge
+            
+
+    except AttributeError:
+        return
+
+
 @receiver(post_save, sender=User)
 def profile_signal(sender, instance, created, **kwargs):
     if created:
         if instance.is_staff is False and instance.is_superuser is False:
             Voters.objects.create(voter=instance)
+        
+        elif instance.is_staff is True and instance.is_superuser is False:
+            Officials.objects.create(officer=instance)
+
