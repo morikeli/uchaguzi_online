192e6ca61a5fa83d753b6c81e4e95ce37cef8c1c
brocode <kelimbth@gmail.com>
2023-02-02 23:11:04 +0300

    --amend

--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,4 @@
+from django.contrib.messages import constants as messages
 from pathlib import Path
 import os
 
@@ -109,22 +110,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR/'static',
-#     BASE_DIR/'static/landing-page',
+STATICFILES_DIRS = [
+    BASE_DIR, 'static',
     
-# ]
-STATIC_ROOT = BASE_DIR/'static/'
+]
+# STATIC_ROOT = BASE_DIR/'static/'
 
 MEDIA_URL = 'mediaFiles/'
-MEDIA_ROOT = 'home/brocode254/uchaguzi_online/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR/'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
