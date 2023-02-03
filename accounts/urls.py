c491fa8bec0e1ce77d83ca2fa94b6dabe83f870f
brocode <kelimbth@gmail.com>
2023-02-03 10:54:59 +0300

    updated views and templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('login/', views.user_login_view, name='user_login'),
-
-    path('create-account/', views.signup_view, name='signup'),
+    path('create-account/', views.voters_signup_view, name='signup'),
+    path('create-official-account/', views.officials_signup_view, name='signup'),
     path('logged-out', views.LogoutUser.as_view(), name='logout_user')
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import auth
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib import messages
 from .forms import LoginForm, SignupForm
 from .models import Voters, Officials
 
@@ -9,8 +10,8 @@ def user_login_view(request):
     form = AuthenticationForm()
 
     if request.method == 'POST':
-        form = AuthenticationForm(request.POST)
-
+        form = AuthenticationForm(data=request.POST)
+        
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
@@ -29,28 +30,43 @@ def user_login_view(request):
                     return redirect('voters_homepage')
 
                 else:
-                    messages.error(request, 'INVALID CREDENTIALS!! Name and password maybe case-sensitive.')
-                    return redirect('')
+                    messages.error(request, 'INVALID CREDENTIALS!!')
+                    return redirect('user_login')
     
 
     context = {'form': form}
     return render(request, 'accounts/login.html', context)
 
 
-def signup_view(request):
+def voters_signup_view(request):
     form = SignupForm()
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
             new_voter = form.save(commit=False)
-            new_voter.username = new_voter.first_name+' '+new_voter.last_name
+            new_voter.username = new_voter.first_name + ' ' + new_voter.last_name
             new_voter.save()
 
-            messages.success(request, 'New account created successfully!')
+            messages.success(request, 'User account created successfully!')
             return redirect('voters_profile')
 
     context = {'signup_form': form}
     return render(request, 'accounts/signup.html', context)
+
+
+def officials_signup_view(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            new_official_account = form.save(commit=False)
+            new_official_account.username = new_official_account.first_name + ' ' + new_official_account.last_name
+            new_official_account.save()
+
+            messages.success(request, 'Electoral official account created successfully!')
+            return redirect('official_profile')
+
+    context = {'signup_form': form}
+    return render(request, 'accounts/officials-signup.html', context)
 
 class LogoutUser(LogoutView):
     template_name = 'accounts/logout.html'
