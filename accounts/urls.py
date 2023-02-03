a6ed92e3e65ae6b3c7808dc22bb3e2f34762f0cc
brocode <kelimbth@gmail.com>
2023-02-03 03:37:03 +0300

    added new views and url paths

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.VoterLogin.as_view(), name='voters_login'),
-    
-    path('create-account/', views.signup_view, name='voters_signup'),
-    path('logout/logged-out', views.LogoutVoter.as_view(), name='logout_voters')
+    path('login/', views.user_login_view, name='user_login'),
+
+    path('create-account/', views.signup_view, name='signup'),
+    path('logged-out', views.LogoutUser.as_view(), name='logout_user')
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,41 @@
 from django.shortcuts import render
+from django.contrib.auth.models import auth
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from .forms import LoginForm, SignupForm
+from .models import Voters, Officials
 
+def user_login_view(request):
+    form = AuthenticationForm()
 
+    if request.method == 'POST':
+        form = AuthenticationForm(request.POST)
 
-class VoterLogin(LoginView):
-    authentication_form = LoginForm
-    template_name = 'accounts/login.html'
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
 
-# class OfficialsLoginView(LoginView):
-#     authentication_form = LoginForm
-#     template_name
+            user_account = auth.authenticate(username=username, password=password)
+
+            if user_account is not None:
+                officer = Officials.objects.filter(officer=username).exists()
+                if officer is True:
+                    if offcier.is_staff is True and officer.is_official is True:
+                        auth.login(request, user_account)
+                        return redirect('officials_homepage')
+
+                elif Voters.objects.filter(voter=username).exists():
+                    auth.login(request, user_account)
+                    return redirect('voters_homepage')
+
+                else:
+                    messages.error(request, 'INVALID CREDENTIALS!! Name and password maybe case-sensitive.')
+                    return redirect('')
+    
+
+    context = {'form': form}
+    return render(request, 'accounts/login.html', context)
+
 
 def signup_view(request):
     form = SignupForm()
@@ -25,10 +50,7 @@ def signup_view(request):
             return redirect('voters_profile')
 
     context = {'signup_form': form}
-    return render(request, 'voters/signup.html', context)
+    return render(request, 'accounts/signup.html', context)
 
-
-
-
-class LogoutVoter(LogoutView):
-    template_name = 'voters/logout.html'
+class LogoutUser(LogoutView):
+    template_name = 'accounts/logout.html'
