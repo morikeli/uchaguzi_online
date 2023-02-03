09498ce2b1ddff26304aecc6a41ed4f22ecd6df9
brocode <kelimbth@gmail.com>
2023-02-03 11:20:20 +0300

    edited conflicting url names

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('login/', views.user_login_view, name='user_login'),
     path('create-account/', views.voters_signup_view, name='signup'),
-    path('create-official-account/', views.officials_signup_view, name='signup'),
+    path('create-official-account/', views.officials_signup_view, name='official_signup'),
     path('logged-out', views.LogoutUser.as_view(), name='logout_user')
 
 ]
