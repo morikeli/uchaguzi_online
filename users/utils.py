e5d95f1dd5fd84c9025ddd7ea7edf69e4663d086
brocode <kelimbth@gmail.com>
2023-02-27 15:33:08 +0300

    added a utils.py file

---
+++ users/utils.py
@@ -0,0 +1,14 @@
+import matplotlib as plt
+import base64
+from io import BytesIO
+
+def get_graph():
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    image_png = buffer.get_value()
+    graph = base64.b64encode(image_png)
+    graph = graph.decode('utf-8')
+    buffer.close()
+    
+    return graph
