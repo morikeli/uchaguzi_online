8ec31c69e6511857cf04ffc5206056e5e244b9ac
brocode <kelimbth@gmail.com>
2023-02-27 23:38:23 +0300

    minor change

--- src/settings.py
+++ src/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
     'crispy_forms',
 ]
 
-CRISPY_TEMPLATE_PACK = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
