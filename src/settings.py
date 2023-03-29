9ac0572401bae89f94ad8a1a98da19e35be8064b
brocode <kelimbth@gmail.com>
2023-03-30 01:22:56 +0300

    minor changes

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,7 @@
 from django.contrib.messages import constants as messages
 from pathlib import Path
 import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,10 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR/'static/'
+STATIC_ROOT = os.path.join(BASE_DIR/'static/')
 
 MEDIA_URL = 'mediaFiles/'
-MEDIA_ROOT = BASE_DIR/'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR/'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
