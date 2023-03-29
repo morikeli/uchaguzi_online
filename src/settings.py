e0e1028320f9a63049729c2017c75671d423b72f
brocode <kelimbth@gmail.com>
2023-03-30 02:09:53 +0300

    updated config

--- src/settings.py
+++ src/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['uchaguzionline-production.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['.now.sh', 'uchaguzionline-production.up.railway.app', '127.0.0.1']
 
 
 # Application definition
