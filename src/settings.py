32558fd530569198ec9c48cbeb2febf7afd3f12c
brocode <kelimbth@gmail.com>
2023-03-30 02:17:02 +0300

    minor changes

--- src/settings.py
+++ src/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.now.sh', 'uchaguzionline-production.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['uchaguzionline-production.up.railway.app', '127.0.0.1']
 
 
 # Application definition
@@ -39,13 +39,13 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'src.urls'
