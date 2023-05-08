0819084331b54935669fc34bd5bca05a9aa4f7af
brocode <kelimbth@gmail.com>
2023-05-08 17:36:27 +0300

    updated ALLOWED_HOSTS

--- src/settings.py
+++ src/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['uchaguzionline-production.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = [env('WEB_DOMAIN')]
 
 
 # Application definition
