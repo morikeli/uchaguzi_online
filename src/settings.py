0ed429f48cd348c3f3ef143b945a43ba76158a3e
brocode <kelimbth@gmail.com>
2023-05-08 17:15:28 +0300

    updated db config.

--- src/settings.py
+++ src/settings.py
@@ -74,12 +74,8 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
